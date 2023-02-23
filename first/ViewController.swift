ff4f5da3dcecdd239c12054d1ce41aa2c7a8657c
Max <sleynip@inbox.ru>
2023-02-24 00:57:35 +0300

    constraints fix

--- first/ViewController.swift
+++ first/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     private var icon = [
         UIImage(named: "rock"),
         UIImage(named: "paper"),
@@ -23,19 +23,20 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
     }
-        
-    private var imageView: UIImageView = {
+    
+    private var opponentChoice: UIImageView = {
         let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = UIImage(named: "ready")
         return imageView
     }()
     
     private lazy var buttonRock: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.tag = 0
         button.setImage(UIImage(named: "rock"), for: .normal)
         button.center = view.center
-        //button.setTitle("Rock", for: .normal)
         button.setTitleColor(UIColor.systemRed, for: .normal)
         button.addTarget(self, action: #selector(btnTapped), for: .touchUpInside)
         return button
@@ -43,9 +44,9 @@ final class ViewController: UIViewController {
     
     private lazy var buttonPaper: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.tag = 1
         button.setImage(UIImage(named: "paper"), for: .normal)
-        //button.setTitle("Paper", for: .normal)
         button.setTitleColor(UIColor.systemRed, for: .normal)
         button.addTarget(self, action: #selector(btnTapped), for: .touchUpInside)
         return button
@@ -53,17 +54,17 @@ final class ViewController: UIViewController {
     
     private lazy var buttonShear: UIButton = {
         let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.tag = 2
         button.setImage(UIImage(named: "shear"), for: .normal)
-        //button.setTitle("Shear", for: .normal)
         button.setTitleColor(UIColor.systemRed, for: .normal)
         button.addTarget(self, action: #selector(btnTapped), for: .touchUpInside)
         return button
     }()
     
-    
     private let labelInfo: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Press icon to start"
         label.textAlignment = .center
         return label
@@ -71,6 +72,7 @@ final class ViewController: UIViewController {
     
     private let labelCountOfWin: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Win: 0"
         label.textAlignment = .center
         return label
@@ -78,6 +80,7 @@ final class ViewController: UIViewController {
     
     private let labelCountOfDraw: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Draw: 0"
         label.textAlignment = .center
         return label
@@ -85,6 +88,7 @@ final class ViewController: UIViewController {
     
     private let labelCountOfLose: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Lose: 0"
         label.textAlignment = .center
         return label
@@ -93,13 +97,13 @@ final class ViewController: UIViewController {
 
 private extension ViewController {
     @objc func btnTapped(sender: UIButton) {
-        let btnsendtag:UIButton = sender
+        let btnSendTag:UIButton = sender
         
-        let randomInt = Int.random(in: 0...2)
-        imageView.image = icon[randomInt]
+        let randomInt = Int.random(in: 0 ..< icon.count)
+        opponentChoice.image = icon[randomInt]
         
-        if btnsendtag.tag == 0 {
-            labelInfo.text = "rock"
+        switch btnSendTag.tag {
+        case 0:
             if randomInt == 2 {
                 labelInfo.text = "rock and shear == win"
                 countOfWin += 1
@@ -110,10 +114,7 @@ private extension ViewController {
                 labelInfo.text = "rock and rock == draw"
                 countOfDraw += 1
             }
-        }
-        
-        if btnsendtag.tag == 1 {
-            labelInfo.text = "paper"
+        case 1:
             if randomInt == 1 {
                 labelInfo.text = "paper and paper == draw"
                 countOfDraw += 1
@@ -124,10 +125,7 @@ private extension ViewController {
                 labelInfo.text = "paper and shear == lose"
                 countOfLose += 1
             }
-        }
-        
-        if btnsendtag.tag == 2 {
-            labelInfo.text = "shear"
+        default:
             if randomInt == 2 {
                 labelInfo.text = "shear and shear == draw"
                 countOfDraw += 1
@@ -139,57 +137,51 @@ private extension ViewController {
                 countOfWin += 1
             }
         }
-        
+        setLabel()
+    }
+    
+    func setLabel() {
         labelCountOfWin.text = "Win: \(countOfWin)"
         labelCountOfDraw.text = "Draw: \(countOfDraw)"
         labelCountOfLose.text = "Lose: \(countOfLose)"
     }
-
+    
     func setupUI() {
         view.backgroundColor = .white
         view.addSubview(labelInfo)
         view.addSubview(buttonRock)
         view.addSubview(buttonPaper)
         view.addSubview(buttonShear)
-        view.addSubview(imageView)
+        view.addSubview(opponentChoice)
         view.addSubview(labelCountOfWin)
         view.addSubview(labelCountOfDraw)
         view.addSubview(labelCountOfLose)
-
-        labelInfo.translatesAutoresizingMaskIntoConstraints = false
-        buttonRock.translatesAutoresizingMaskIntoConstraints = false
-        buttonPaper.translatesAutoresizingMaskIntoConstraints = false
-        buttonShear.translatesAutoresizingMaskIntoConstraints = false
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        labelCountOfWin.translatesAutoresizingMaskIntoConstraints = false
-        labelCountOfDraw.translatesAutoresizingMaskIntoConstraints = false
-        labelCountOfLose.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             labelInfo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
             labelInfo.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             labelInfo.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            imageView.topAnchor.constraint(equalTo: labelInfo.bottomAnchor, constant: 25),
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            opponentChoice.topAnchor.constraint(equalTo: labelInfo.bottomAnchor, constant: 25),
+            opponentChoice.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-            buttonRock.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 25),
-            
-            buttonPaper.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 50),
-            buttonPaper.leftAnchor.constraint(equalTo: buttonRock.leftAnchor, constant: 100),
+            buttonPaper.topAnchor.constraint(equalTo: opponentChoice.bottomAnchor, constant: 50),
             buttonPaper.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
-            buttonShear.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 25),
-            buttonShear.leftAnchor.constraint(equalTo: buttonPaper.leftAnchor, constant: 100),
+            buttonRock.topAnchor.constraint(equalTo: opponentChoice.bottomAnchor, constant: 50),
+            buttonRock.trailingAnchor.constraint(equalTo: buttonPaper.leadingAnchor, constant: -50),
+                        
+            buttonShear.topAnchor.constraint(equalTo: opponentChoice.bottomAnchor, constant: 50),
+            buttonShear.leadingAnchor.constraint(equalTo: buttonPaper.trailingAnchor, constant: 50),
             
             labelCountOfWin.topAnchor.constraint(equalTo: buttonPaper.bottomAnchor, constant: 50),
-            labelCountOfWin.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-            labelCountOfDraw.topAnchor.constraint(equalTo: buttonPaper.bottomAnchor, constant: 50),
-            labelCountOfDraw.rightAnchor.constraint(equalTo: labelCountOfWin.rightAnchor, constant: 100),
+            labelCountOfWin.centerXAnchor.constraint(equalTo: buttonPaper.centerXAnchor),
             
             labelCountOfLose.topAnchor.constraint(equalTo: buttonPaper.bottomAnchor, constant: 50),
-            labelCountOfLose.rightAnchor.constraint(equalTo: labelCountOfWin.rightAnchor, constant: -100),
+            labelCountOfLose.centerXAnchor.constraint(equalTo: buttonRock.centerXAnchor),
+            
+            labelCountOfDraw.topAnchor.constraint(equalTo: buttonPaper.bottomAnchor, constant: 50),
+            labelCountOfDraw.centerXAnchor.constraint(equalTo: buttonShear.centerXAnchor),
 
         ])
     }
